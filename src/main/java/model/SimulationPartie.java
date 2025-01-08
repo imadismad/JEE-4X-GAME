89bb4a3d0591f9373c20dce4ce4ccd29ae00d344
Nathan FOUCHET <nathan.fouchet@gendarmerie.interieur.gouv.fr>
2025-01-08 21:55:30 +0100

    change simu

--- src/main/java/model/SimulationPartie.java
+++ src/main/java/model/SimulationPartie.java
@@ -12,19 +12,19 @@ public class SimulationPartie {
         Utilisateur utilisateur1 = new Utilisateur("Joueur1", null);
         Utilisateur utilisateur2 = new Utilisateur("Joueur2", null);
         Utilisateur utilisateur3 = new Utilisateur("Joueur3", null);
-        Utilisateur utilisateur4 = new Utilisateur("Joueur4", null);
+//        Utilisateur utilisateur4 = new Utilisateur("Joueur4", null);
 
         // Associer les utilisateurs à des joueurs
         Joueur joueur1 = new Joueur(utilisateur1, partie);
         Joueur joueur2 = new Joueur(utilisateur2, partie);
         Joueur joueur3 = new Joueur(utilisateur3, partie);
-        Joueur joueur4 = new Joueur(utilisateur4, partie);
+//        Joueur joueur4 = new Joueur(utilisateur4, partie);
 
         // Ajouter les joueurs à la partie
         partie.ajouterJoueur(joueur1);
         partie.ajouterJoueur(joueur2);
         partie.ajouterJoueur(joueur3);
-        partie.ajouterJoueur(joueur4);
+//        partie.ajouterJoueur(joueur4);
 
         // Initialiser la carte
         partie.initialiserCarte();
